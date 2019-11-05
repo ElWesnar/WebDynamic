e45d4e3feee00a008973b09abe21d72913f9f32a
RobertPadfiote <nicolas.wasner@gmail.com>
2019-11-05 16:09:17 +0100

    add director.php

---
+++ director.php
@@ -0,0 +1,18 @@
+<?php
+echo "<!DOCTYPE html>
+<html lang=\"en\">
+<head>
+    <meta charset=\"UTF-8\">
+    <meta name=\"viewport\">
+    <title>director</title>
+    <link rel=\"stylesheet\" href=\"style.css\">
+</head>
+<body>";
+include 'php/infosDirector.php';
+include 'php/filmographie.php';
+include 'php/presD.php';
+
+
+
+echo"</body>
+</html>";
---
+++ php/presD.php
@@ -0,0 +1,7 @@
+<?php
+echo"<div class=\"asided\">
+    <figure class=\"Actor\">
+        <figcaption>Michael Tiddes(Réalisateur)</figcaption>
+        <img src=\"img/realisateur.jpg\" alt=\"Actor (Michael Tiddes) face picture\">
+    </figure>
+</div>";
